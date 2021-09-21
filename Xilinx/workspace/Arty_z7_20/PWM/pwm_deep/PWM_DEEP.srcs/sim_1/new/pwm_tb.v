4f51e9045331b6f4e58e20e5c859a70304bd6bbc
Kouta Kimura <afunfun5100@gmail.com>
2021-09-21 21:09:18 +0900

    [update] PWMのプロジェクト追加
    追加の際にgitignoreに追記した

---
+++ Xilinx/workspace/Arty_z7_20/PWM/pwm_deep/PWM_DEEP.srcs/sim_1/new/pwm_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/09/21 20:58:08
+// Design Name: 
+// Module Name: pwm_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_tb(
+
+    );
+endmodule
---
+++ Xilinx/workspace/Arty_z7_20/PWM/pwm_deep/PWM_DEEP.srcs/sources_1/new/pwm_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/09/21 20:50:59
+// Design Name: 
+// Module Name: pwm_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_top(
+
+    );
+endmodule
