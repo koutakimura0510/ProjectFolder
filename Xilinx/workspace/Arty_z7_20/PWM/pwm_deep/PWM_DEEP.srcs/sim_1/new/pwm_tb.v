2efabd92592f685cb4eab6a612de9e574f33eafe
Kouta Kimura <afunfun5100@gmail.com>
2021-09-22 09:39:13 +0900

    [update] テストベンチの作成

--- Xilinx/workspace/Arty_z7_20/PWM/pwm_deep/PWM_DEEP.srcs/sim_1/new/pwm_tb.v
+++ Xilinx/workspace/Arty_z7_20/PWM/pwm_deep/PWM_DEEP.srcs/sim_1/new/pwm_tb.v
@@ -1,26 +1,47 @@
+/*
+ * Create 2021/09/21
+ * Author kimura
+ * OS     Ubuntu LTS 20.04
+ * Editor VSCode ver1.57.1
+ * Build  vivado20.2
+ * Borad  ArtyZ7-20
+ * -
+ * pwm動作確認テストベンチ
+ */
+
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/09/21 20:58:08
-// Design Name: 
-// Module Name: pwm_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module pwm_tb;
+
+/* comment */
+parameter CYCLE = 10;
+
+integer count = 0;
+
+reg reset, clk;
+wire pwm, pwm_debug, led;
+
+defparam pwm_top.MAX_CLK = 4;
+pwm_top i0(
+    .CLK(clk),
+    .RESET(reset),
+    .PWM(pwm),
+    .pwm_debug(pwm_debug),
+    .LED(led),
+);
+
+always #(CYCLE/2)
+	clk = ~clk;
 
 
-module pwm_tb(
+initial begin
+    clk   = 0;
+    reset = 0;
+    #(CYCLE);
+    reset = 1;
+    #(CYCLE * 16);
+    $finish;
+end
 
-    );
+
+
 endmodule
