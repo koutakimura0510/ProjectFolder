f6c8a8053655a5c20f1b0b763fea68e5ab9ecf56
Kouta Kimura <afunfun5100@gmail.com>
2021-09-28 09:29:52 +0900

    [update] コメント追加

--- Xilinx/workspace/Arty_z7_20/HDMI/HDMI_VGA/HDMI_DMA.vitis/RPG_System_ver1/src/system/wrapper/npc_wrapper.h
+++ Xilinx/workspace/Arty_z7_20/HDMI/HDMI_VGA/HDMI_DMA.vitis/RPG_System_ver1/src/system/wrapper/npc_wrapper.h
@@ -23,9 +23,18 @@
  * 左に2マス：右に2マス：上に3マス：下に3マスだけ移動できるよ。
  * dir_timeの時間で移動量の処理を行うことになるね。
  * 
+ * pixel_adjust
+ * NPCの座標更新時にpixel移動の処理を入れないと、1マスを瞬間移動したようになってしまう。
+ * そのため進んだ方向とは逆数の値を保存し、pixel数を移動量分加算または減算しなければならない。
+ * 右 = -32
+ * 左 = 32
+ * 上 = 32
+ * 下 = -32
+ * 
  * @param  map_npcid マップが保持するNPCの連番を保存
  * @param  id npc自身が持つIDを保存
  * @param  map_pos[] NPCのx,y座標の移動量を保存、[0][]=xの移動量を保存。[1][]=yの移動量を保存
+ * @param  pixel_adjust[] アニメーション時の移動座標を管理
  * @param  dram_index マップデータのNPCの初期座標を保存
  * @param  mapchip_id 描画を行うマップチップのIDを保存
  * @param  cut_pos マップチップの切り取り箇所を保存
@@ -41,8 +50,7 @@ typedef struct npc_wrapper
     uint32_t map_npcid[NPC_MAX_DRAW_NUM];
     uint32_t id[NPC_MAX_DRAW_NUM];
     int8_t   map_pos[NPC_INDEX_NUMBER][NPC_MAX_DRAW_NUM];
-    int32_t  x[NPC_MAX_DRAW_NUM];
-    int32_t  y[NPC_MAX_DRAW_NUM];
+    int32_t  pixel_adjust[NPC_INDEX_NUMBER][NPC_MAX_DRAW_NUM];
     uint32_t dram_index[NPC_MAX_DRAW_NUM];
     uint8_t  mapchip_id[NPC_MAX_DRAW_NUM];
     uint8_t  cut_pos[NPC_MAX_DRAW_NUM];
