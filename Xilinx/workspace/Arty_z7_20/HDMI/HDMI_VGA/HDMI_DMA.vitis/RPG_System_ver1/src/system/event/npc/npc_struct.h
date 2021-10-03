79e62e7f612f2b48f932034bb2dfc119bc87e1e8
Kouta Kimura <afunfun5100@gmail.com>
2021-10-04 08:15:35 +0900

    [npc_struct.h] 関数テーブルをnpc.cに移動

--- Xilinx/workspace/Arty_z7_20/HDMI/HDMI_VGA/HDMI_DMA.vitis/RPG_System_ver1/src/system/event/npc/npc_struct.h
+++ Xilinx/workspace/Arty_z7_20/HDMI/HDMI_VGA/HDMI_DMA.vitis/RPG_System_ver1/src/system/event/npc/npc_struct.h
@@ -7,23 +7,3 @@
  * -
  * @brief npcの描画に関する状態遷移の構造体をまとめたファイル
  */
-
-
-/* NPCのアニメーション処理時の座標・向き更新関数 */
-static void npc_dir_update(GameWrapper *const game, uint8_t index);
-static void npc_pixel_update(GameWrapper *const game, uint8_t index);
-
-
-/**
- * @brief  NPCのアニメーション状態によってメンバの更新関数を変更する関数ポインタ
- * @note   
- * @retval None
- */
-typedef struct
-{
-	void (*const npc_position[NPC_ACTIVE_NUMBER])(GameWrapper *const game, uint8_t index);
-} NpcPosition;
-
-static const NpcPosition npc_position = {
-	{npc_dir_update, npc_pixel_update},
-};
