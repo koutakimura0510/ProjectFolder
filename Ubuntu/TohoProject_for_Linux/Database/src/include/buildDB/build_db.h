ea9ace0bd747a9cb3083c28c743f6d389e5d5144
Kouta Kimura <afunfun5100@gmail.com>
2021-10-09 20:27:39 +0900

    [build_db] バグ修正
    データベースの列の並び順が規定通りではなかったため修正した

--- Ubuntu/TohoProject_for_Linux/Database/src/include/buildDB/build_db.h
+++ Ubuntu/TohoProject_for_Linux/Database/src/include/buildDB/build_db.h
@@ -13,7 +13,7 @@
 /* データベースの行数 */
 #define BUILD_EVENT_POS_DB_SIZE ((sizeof (build_event_pos))/(sizeof (BuildEventPos)))
 #define BUILD_EVENT_DB_SIZE ((sizeof (build_event))/(sizeof (BuildEvent)))
-#define BUILD_MOVE_DB_SIZE ((sizeof (build_move))/(sizeof (BuildMove)))
+#define BUILD_SUBMAP_DB_SIZE ((sizeof (build_submap))/(sizeof (BuildSubmap)))
 
 
 /**
@@ -27,29 +27,29 @@
  */
 typedef struct build_event_pos
 {
+	uint32_t event_id;
 	uint32_t xpos;
 	uint32_t ypos;
-	uint32_t event_id;
 	uint32_t isVariable;
 } BuildEventPos;
 
 static const BuildEventPos build_event_pos[] =
 {
-	{0,  0,		DIRECT_ROMEN_ID_EXIT,			EVENT_VARIABLE	},
-	{41, 20, 	DIRECT_ROMEN_ID_HOUSE1,			EVENT_FIXED	},
-	{11, 12, 	DIRECT_ROMEN_ID_HOUSE2,			EVENT_FIXED	},
-	{10, 10,	DIRECT_ROMEN_ID_ITEM_SHOP,		EVENT_FIXED	},
-	{10, 10,	DIRECT_ROMEN_ID_WEPON_SHOP,		EVENT_FIXED	},/* テーブルの向こう側にいる場合処理 */
-	{18, 11,	DIRECT_ROMEN_ID_ITEM1,			EVENT_FIXED	},
-	{18, 39,	DIRECT_ROMEN_ID_ITEM2,			EVENT_FIXED	},
-	{30, 11,	DIRECT_ROMEN_ID_ITEM3,			EVENT_FIXED	},
-	{17, 41,	DIRECT_ROMEN_ID_KANBAN1,		EVENT_FIXED	},
-	{31, 26,	DIRECT_ROMEN_ID_KANBAN3_1,		EVENT_FIXED	},
-	{32, 26,	DIRECT_ROMEN_ID_KANBAN3_2,		EVENT_FIXED	},
-	{24, 15,	DIRECT_ROMEN_ID_KANBAN4,		EVENT_FIXED	},
-	{29, 15,	DIRECT_ROMEN_ID_KANBAN5,		EVENT_FIXED	},
-	{12, 10,	DIRECT_ROMEN_ID_LOOM1,			EVENT_FIXED	},
-	{12, 10,	DIRECT_ROMEN_ID_LOOM2,			EVENT_FIXED	},
+	{DIRECT_ROMEN_ID_EXIT,			0,  0, 		EVENT_VARIABLE	},
+	{DIRECT_ROMEN_ID_HOUSE1,		41, 20, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_HOUSE2,		11, 12, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_KANBAN1,		17, 41, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_KANBAN3_1,		31, 26, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_KANBAN3_2,		32, 26, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_KANBAN4,		24, 15, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_KANBAN5,		29, 15, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_ITEM_SHOP,		10, 10, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_WEPON_SHOP,	10, 10, 	EVENT_FIXED		}, /* テーブルの向こう側にいる場合処理 */
+	{DIRECT_ROMEN_ID_ITEM1,			18, 11, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_ITEM2,			18, 39, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_ITEM3,			30, 11, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_LOOM1,			12, 10, 	EVENT_FIXED		},
+	{DIRECT_ROMEN_ID_LOOM2,			12, 10, 	EVENT_FIXED		},
 };
 
 
@@ -80,16 +80,16 @@ static const BuildEvent build_event[] =
 	{DIRECT_ROMEN_ID_EXIT,	 	 EVENT_TYPE_WORLD_STATE_OTHER_MAP, 	TRY_ROMEN_EXIT_ID, SYSTEM_MAP_DARKENING_WINDOW,	DISPLAY_DARKENING_DRAW,		},
 	{DIRECT_ROMEN_ID_HOUSE1,	 EVENT_TYPE_SAMEMAP_MOVE,  			TRY_MAP_NAME_NULL, SYSTEM_MAP_DARKENING_SUBMAP,	DISPLAY_DARKENING_DRAW,    	},
 	{DIRECT_ROMEN_ID_HOUSE2,	 EVENT_TYPE_SAMEMAP_MOVE,  			TRY_MAP_NAME_NULL, SYSTEM_MAP_DARKENING_SUBMAP,	DISPLAY_DARKENING_DRAW,    	},
-	{DIRECT_ROMEN_ID_ITEM_SHOP,  EVENT_TYPE_TOOLS_SHOP, 			TRY_MAP_NAME_NULL, SYSTEM_SHOP_WINDOW,			DISPLAY_FIELD_CENTER_DRAW,	},
-	{DIRECT_ROMEN_ID_WEPON_SHOP, EVENT_TYPE_TOOLS_SHOP, 			TRY_MAP_NAME_NULL, SYSTEM_SHOP_WINDOW,			DISPLAY_FIELD_CENTER_DRAW,	},/* テーブルの向こう側にいる場合処理 */
-	{DIRECT_ROMEN_ID_ITEM1,	 	 EVENT_TYPE_TREASURE_GET,  			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
-	{DIRECT_ROMEN_ID_ITEM2,	 	 EVENT_TYPE_TREASURE_GET,  			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
-	{DIRECT_ROMEN_ID_ITEM3,	 	 EVENT_TYPE_ITEM_GET,   			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
 	{DIRECT_ROMEN_ID_KANBAN1, 	 EVENT_TYPE_MSG_TORK,   			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
 	{DIRECT_ROMEN_ID_KANBAN3_1,	 EVENT_TYPE_MSG_TORK,   			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
 	{DIRECT_ROMEN_ID_KANBAN3_2,	 EVENT_TYPE_MSG_TORK,   			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
 	{DIRECT_ROMEN_ID_KANBAN4, 	 EVENT_TYPE_MSG_TORK,   			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
 	{DIRECT_ROMEN_ID_KANBAN5, 	 EVENT_TYPE_MSG_TORK,   			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
+	{DIRECT_ROMEN_ID_ITEM_SHOP,  EVENT_TYPE_TOOLS_SHOP, 			TRY_MAP_NAME_NULL, SYSTEM_SHOP_WINDOW,			DISPLAY_FIELD_CENTER_DRAW,	},
+	{DIRECT_ROMEN_ID_WEPON_SHOP, EVENT_TYPE_TOOLS_SHOP, 			TRY_MAP_NAME_NULL, SYSTEM_SHOP_WINDOW,			DISPLAY_FIELD_CENTER_DRAW,	},/* テーブルの向こう側にいる場合処理 */
+	{DIRECT_ROMEN_ID_ITEM1,	 	 EVENT_TYPE_TREASURE_GET,  			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
+	{DIRECT_ROMEN_ID_ITEM2,	 	 EVENT_TYPE_TREASURE_GET,  			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
+	{DIRECT_ROMEN_ID_ITEM3,	 	 EVENT_TYPE_ITEM_GET,   			TRY_MAP_NAME_NULL, SYSTEM_MSG_WINDOW,			DISPLAY_FIELD_CENTER_DRAW, 	},
 	{DIRECT_ROMEN_ID_LOOM1,	 	 EVENT_TYPE_SAMEMAP_MOVE,  			TRY_MAP_NAME_NULL, SYSTEM_MAP_DARKENING_SUBMAP,	DISPLAY_DARKENING_DRAW, 	},
 	{DIRECT_ROMEN_ID_LOOM2,	 	 EVENT_TYPE_SAMEMAP_MOVE,  			TRY_MAP_NAME_NULL, SYSTEM_MAP_DARKENING_SUBMAP,	DISPLAY_DARKENING_DRAW, 	},
 };
@@ -109,7 +109,7 @@ static const BuildEvent build_event[] =
  * サブマップ間移動のため、家１の部屋１に移動IDを更新する。
  * 部屋１の出口と当たり判定があった場合は、ロメン村の全体マップに移動したとみなし、そのデータを取得する。
  */
-typedef struct build_move
+typedef struct build_submap
 {
 	uint32_t event_id;
 	uint32_t next_file_mapdata_id;
@@ -121,9 +121,9 @@ typedef struct build_move
 	uint8_t unity;
 	uint8_t fieldx;
 	uint8_t fieldy;
-} BuildMove;
+} BuildSubmap;
 
-static const BuildMove build_move[] =
+static const BuildSubmap build_submap[] =
 {
 	{DIRECT_ROMEN_ID_HOUSE1, FILE_ACCESS_MAPDATA_ROMEN_HOUSE1,  FILE_ACCESS_MAPDATA_ROMEN_HOUSE1_OBJECT,  FILE_ACCESS_MAPDATA_ROMEN_HOUSE1_REGION, FILE_ACCESS_MAPDATA_ROMEN_HOUSE1_NPC, ROMEN_HOUSE1_LOOM1_ID, 	10, 1, 0, 0},
 	{DIRECT_ROMEN_ID_HOUSE2, FILE_ACCESS_MAPDATA_ROMEN_HOUSE2,  FILE_ACCESS_MAPDATA_ROMEN_HOUSE2_OBJECT,  FILE_ACCESS_MAPDATA_ROMEN_HOUSE2_REGION, FILE_ACCESS_MAPDATA_ROMEN_HOUSE2_NPC, ROMEN_HOUSE2_LOOM1_ID, 	10, 7, 0, 0},
@@ -135,8 +135,6 @@ static const BuildMove build_move[] =
 /**
  * @brief  固定イベント座標データ書き出し
  * @note   
- * @param  *fp: 
- * @param  *byte: 
  * @retval None
  */
 void build_pos_write(FILE *fp, FILE *byte)
@@ -145,9 +143,9 @@ void build_pos_write(FILE *fp, FILE *byte)
 
 	for (uint32_t i = 0; i < BUILD_EVENT_POS_DB_SIZE; i++, p++)
 	{
+		fprintf(fp, "0x%08x,\n", p->event_id);
 		fprintf(fp, "0x%08x,\n", p->xpos);
 		fprintf(fp, "0x%08x,\n", p->ypos);
-		fprintf(fp, "0x%08x,\n", p->event_id);
 		fprintf(fp, "0x%08x,\n", p->isVariable);
 
 		for (uint32_t j = 0; j < BUILD_POS_MEMBER_NUMBER; j++)
@@ -163,8 +161,6 @@ void build_pos_write(FILE *fp, FILE *byte)
 /**
  * @brief  固定イベントデータ書き出し
  * @note   
- * @param  *fp: 
- * @param  *byte: 
  * @retval None
  */
 void build_event_write(FILE *fp, FILE *byte)
@@ -192,15 +188,13 @@ void build_event_write(FILE *fp, FILE *byte)
 /**
  * @brief  建物内のサブマップ情報書き出し
  * @note   
- * @param  *fp: 
- * @param  *byte: 
  * @retval None
  */
 void build_submap_write(FILE *fp, FILE *byte)
 {
-	const BuildMove *p = build_move;
+	const BuildSubmap *p = build_submap;
 
-	for (uint32_t i = 0; i < BUILD_MOVE_DB_SIZE; i++, p++)
+	for (uint32_t i = 0; i < BUILD_SUBMAP_DB_SIZE; i++, p++)
 	{
 		fprintf(fp, "0x%08x,\n", p->event_id);
 		fprintf(fp, "0x%08x,\n", p->next_file_mapdata_id);
@@ -219,7 +213,7 @@ void build_submap_write(FILE *fp, FILE *byte)
 		}
 	}
 
-	error_print(BUILD_MOVE_DB_SIZE, 4, "BUILD SUBMAP TOTAL NUMBER");
+	error_print(BUILD_SUBMAP_DB_SIZE, 4, "BUILD SUBMAP TOTAL NUMBER");
 }
 
 
