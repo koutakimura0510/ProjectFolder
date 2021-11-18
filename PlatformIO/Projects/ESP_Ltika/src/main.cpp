19eabae9a659506e630f4c99cdfbed522877d5b1
Kouta Kimura <afunfun5100@icloud.com>
2021-11-18 20:29:20 +0900

    [new Platformio] ESPのサンプルプロジェクト追加

---
+++ PlatformIO/Projects/ESP_Ltika/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "SSD1306.h"
+
+SSD1306 display(0x3c, 21, 22);
+int led = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  display.init();
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 0, "Hello World");
+  display.display();
+}
+
+void loop() {
+  digitalWrite(4, led);
+  led ^= 1;
+  delay(1000);
+}
