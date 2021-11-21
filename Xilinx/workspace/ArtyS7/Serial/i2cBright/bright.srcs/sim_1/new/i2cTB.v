2ace053b0d67ded225eacf73bb8c891032084808
Kouta Kimura <afunfun5100@icloud.com>
2021-11-21 17:29:40 +0900

    [new I2C] I2Cのプロジェクト追加

---
+++ Xilinx/workspace/ArtyS7/Serial/i2cBright/bright.srcs/sim_1/new/i2cTB.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/11/21 17:27:47
+// Design Name: 
+// Module Name: i2cTB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module i2cTB(
+
+    );
+endmodule
---
+++ Xilinx/workspace/ArtyS7/Serial/i2cBright/bright.srcs/sources_1/new/i2cTop.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/11/21 17:27:29
+// Design Name: 
+// Module Name: i2cTop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module i2cTop(
+
+    );
+endmodule
