f71b3054e6827951170b4785f86ec8aaf57209b8
Kimura <afunfun5100@icloud.com>
2021-11-24 14:43:02 +0900

    [update enGen] parameter追記

--- Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/enGen.v
+++ Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/enGen.v
@@ -9,21 +9,25 @@
  */
 module enGen
 (
+    parameter pCLK = 100000000
+)(
 input 			iRST,		// System Reset
 input 			iCLK,		// System Clock
 output          enKhz
 );
 
-parameter MAX_CNT = (100000000 - 1);    // システムクロック最大カウント
 parameter SEG_CNT = (500000 - 1);       // ダイナミック点灯5ms Enable信号生成
 
 // enable信号の生成
-reg [26:0] tmp_count;       assign enKhz = (tmp_count[18:0] == SEG_CNT) ? 1'b1 : 1'b0;
+reg [26:0] tmp_count;
+
+assign enable = (tmp_count[26:0] == (pCLK - 1)) ? 1'b1 : 1'b0;
+assign enKhz  = (tmp_count[18:0] == SEG_CNT) ? 1'b1 : 1'b0;
 
 always @(posedge iCLK) begin
     if (iRST == 1'b1) begin
         tmp_count <= 27'd0;
-    end else if (MAX_CNT == tmp_count) begin
+    end else if (enable == 1'b1) begin
         tmp_count <= 27'd0;
     end else begin
         tmp_count <= tmp_count + 27'd1;
