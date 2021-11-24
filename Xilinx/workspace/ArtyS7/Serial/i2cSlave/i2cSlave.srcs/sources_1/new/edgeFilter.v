7f2219c4b3480e839cdf933d833597b4fe9f3f9f
Kimura <afunfun5100@icloud.com>
2021-11-24 10:04:49 +0900

    [update edgeFilter] 非同期信号の前段処理追加

--- Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/edgeFilter.v
+++ Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/edgeFilter.v
@@ -9,9 +9,48 @@
  */
 module edgeFilter
 (
-input  iCLK,
-input  iRST,
-input  iSerial,
-output oSerial
+input  		iCLK,
+input  		iRST,
+input  		iSerial,
+output 		oSerial
 );
+
+reg oSerial;
+reg [1:0] meta;		// メタ・ステーブル対策
+reg [2:0] sft;		// ノイズ除去用フィルタ
+wire chatta;		// 3点一致検出用信号
+
+// 2段フリップフロップで受信
+always @(posedge iCLK) begin
+	if (iRST == 1'd1) begin
+		meta <= 2'd0;
+	end else begin
+		meta <= {meta[0], iSerial};
+	end
+end
+
+// チャタリング除去
+always @(posedge iCLK) begin
+	if (iRST == 1'd0) begin
+		sft <= 3'd0;
+	end else begin
+		sft <= {sft[1:0], meta[1]};
+	end
+end
+
+// 比較器、High Low信号が3点一致したら1を出力
+assign chatta = (sft[0] == sft[1] == sft[2]) ? 1'd1 : 1'd0;
+
+// 非同期信号をシステムクロックに同期させて出力
+// リセットがかからなければ前回の出力状態を維持する
+always @(posedge iCLK) begin
+	if (iRST == 1'd1) begin
+		oSerial <= 1'd0;
+	end else begin
+		if (chata == 1'd1) begin
+			oSerial <= sft[2];
+		end
+	end
+end
+
 endmodule
