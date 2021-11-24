1980cd56f2d716868642b63ceccf583929a2f10b
Kimura <afunfun5100@icloud.com>
2021-11-24 13:49:59 +0900

    [update i2cSampling] シリパラ変換処理追加中
    シミュレーションはしていない

--- Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/i2cSampling.v
+++ Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/i2cSampling.v
@@ -14,6 +14,89 @@ input  iCLK,
 input  iRST,
 input  iSCL,
 input  iSDA,
-output i2cByte
+output [7:0] i2cByte
 );
+
+localparam [2:0] 
+	disConnect 		= 3'd0,		// マスタがSCLがHIGHの間にSDAをLOWでStartシーケンス
+	startCondition 	= 3'd1,		// SCLがHIGHの間にSDAをHIGHでStopシーケンス
+	stopCondition 	= 3'd2;
+
+localparam [3:0] 
+	SclCnt 		= 4'd1,
+	SclNull 	= 4'd0,
+	SclMax	 	= 4'd7;
+
+
+// i2c状態管理
+reg [1:0] sftScl;	// sclの状態を管理を行うシフトレジスタ
+reg [1:0] sftSda;	// sdaの状態を管理を行うシフトレジスタ
+reg [2:0] i2cState;	// start stopシーケンサ管理
+
+// i2cシリアルデータ操作
+reg [7:0] sftSel;	// パラレル変換用にシリアルデータを保存するシフトレジスタ
+reg [7:0] i2cByte;	// パラレル出力用
+reg [3:0] sclCnt;	// sdaの受信回数カウント
+wire sclEdge;		// sclの立ち上がり検出
+
+
+
+// 現在のI2Cの状態を確認するためシフトレジスタにクロックのデータを保存する
+always @(posedge iCLK) begin
+	if (iRST == 1'b1) begin
+		sftScl <= 2'd0;
+		sftSda <= 2'd0;
+	end else begin
+		sftScl <= {sftScl[0], iSCL};
+		sftSda <= {sftSda[0], iSDA};
+	end
+end
+
+// start stop condition検出
+always @(posedge iCLK) begin
+	if (iRST == 1'b1) begin
+		i2cState <= disConnect;
+	end else begin
+		case (i2cState)
+			disConnect: 	i2cState <= (sftScl == 2'b11 && sftSda == 2'b00) ? startCondition : disConnect;
+			startCondition:	i2cState <= (sftScl == 2'b11 && sftSda == 2'b01) ? disConnect : startCondition;
+			default:		disConnect;
+		endcase
+	end
+end
+
+// iSCLのエッジ検出
+assign sclEdge = (sftScl == 2'b01) ? 1'b1 : 1'b0;
+
+// sdaシリアルデータ保存
+always @(posedge iCLK) begin
+	if (iRST == 1'b1) begin
+		sftSel <= 8'd0;
+	end else if (sclEdge == 1'b1) begin
+		sftSel <= {sftSel[6:0], iSDA};
+	end
+end
+
+// sclの立ち上がりエッジで受信回数をカウント
+always @(posedge iCLK) begin
+	if (iRST == 1'b1) begin
+		sclCnt <= SclNull;
+	end else if (sclEdge == 1'b1) begin
+		if (sclCnt == SclMax) begin
+			sclCnt <= SclNull;
+		end else begin
+			sclCnt <= sclCnt + SclCnt;
+		end
+	end
+end
+
+// シリアルデータをパラレルレジスタに保存
+always @(posedge iCLK) begin
+	if (iRST == 1'b1) begin
+		i2cByte <= 8'd0;
+	end else if (sclEdge == 1'b1) begin
+		i2cByte <= sftSel;
+	end
+end
+
 endmodule
