467a5ff55bcf9c2bd8d47ead7c6da2a8e2d79bd3
Kouta Kimura <afunfun5100@icloud.com>
2021-11-24 21:25:08 +0900

    [update]

--- Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.runs/impl_1/rundef.js
+++ Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.runs/impl_1/rundef.js
@@ -27,7 +27,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log i2cTop.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source i2cTop.tcl -notrace" );
 
