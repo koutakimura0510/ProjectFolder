502278064354e3616ed15be7ee69550948b2503a
Kouta Kimura <afunfun5100@icloud.com>
2021-11-27 19:47:34 +0900

    [add]

--- Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/xsim_webtalk.tcl
+++ Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Nov 26 20:28:51 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Nov 27 18:43:31 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "185e2bdb-ca6a-4337-a90e-b3845e533580" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "f7cde0ec00654cfa8e02c6639aab0df7" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "35" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "45" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 20.04.3 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz" -context "user_environment"
@@ -23,11 +23,13 @@ webtalk_add_data -client project -key total_processors -value "1" -context "user
 webtalk_add_data -client project -key system_ram -value "33.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
+webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "19800 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.04_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "115624_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 544635978 -regid "" -xml /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "4.56_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "116396_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 872026964 -regid "" -xml /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.sim/sim_1/behav/xsim/xsim.dir/i2cTB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
