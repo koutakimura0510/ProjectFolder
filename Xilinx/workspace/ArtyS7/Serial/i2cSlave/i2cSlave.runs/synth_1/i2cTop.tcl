64e6c0095bc406abb6979742dd6b42dcd3b08acd
Kouta Kimura <afunfun5100@icloud.com>
2021-11-27 20:35:57 +0900

    [add]

--- Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.runs/synth_1/i2cTop.tcl
+++ Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.runs/synth_1/i2cTop.tcl
@@ -90,7 +90,9 @@ OPTRACE "Adding files" START { }
 read_verilog -library xil_defaultlib {
   /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/edgeFilter.v
   /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/enGen.v
+  /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/i2cMaster.v
   /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/i2cSampling.v
+  /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/oledState.v
   /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/pmodDynamic.v
   /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/pmodSeg.v
   /home/koutakimura/workspace/ProjectFolder/Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/i2cTop.v
