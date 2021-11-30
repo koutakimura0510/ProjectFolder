ebf2d245157b2b129f3a001400b652f7bc42fc0f
Kimura <afunfun5100@icloud.com>
2021-11-30 14:52:19 +0900

    [i2cMaster.v] sdaのトライステート処理変更
    ハイインピーダンスで比較していたが、xの不定値で比較したほうが良さそうだったため変更

--- Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/i2cMaster.v
+++ Xilinx/workspace/ArtyS7/Serial/i2cSlave/i2cSlave.srcs/sources_1/new/i2cMaster.v
@@ -46,7 +46,7 @@ localparam [6:0]
 //----------------------------------------------------------
 // i2c信号生成
 reg ioSclf;		assign ioSCLF  = ioSclf;
-reg ioSdaf;		assign ioSDAF  = (ioSdaf == 1'bz) ? 1'bz : ioSdaf;
+reg ioSdaf;		assign ioSDAF  = (ioSdaf == 1'bx) ? 1'bz : ioSdaf;
 reg oenable;	assign oEnable = oenable;
 
 // i2c状態遷移管理変数
@@ -245,7 +245,7 @@ always @(posedge iCLK) begin
 					if (iLength == mLength) begin // 指定バイト送信時、stop conditionに備えてlowにする
 						ioSdaf <= 1'b0;
 					end else if (sclCnt == SclDataByte) begin
-						ioSdaf <= 1'bz;
+						ioSdaf <= 1'bx;
 					end else begin
 						ioSdaf <= sendByte[sdaRp]; //byte data
 					end
