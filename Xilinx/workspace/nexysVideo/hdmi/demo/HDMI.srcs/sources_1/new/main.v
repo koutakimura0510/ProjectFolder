20aa4fd8109a63adb79ef540668e98abea908a91
Kouta Kimura <afunfun5100@icloud.com>
2021-12-22 21:16:35 +0900

    [Nexys Video HDMI new Project]

---
+++ Xilinx/workspace/nexysVideo/hdmi/demo/HDMI.srcs/sources_1/new/main.v
@@ -0,0 +1,17 @@
+/*
+ * Create 2021/12/22
+ * Author koutakimura
+ * Editor VSCode ver1.62.7
+ * Build  Vivado20.2
+ * Borad  Nexys Video
+ * -
+ * HDMIサンプル動作
+ */
+module main
+(
+    input           iCLK,
+    input           iRST,
+    input           iBTNC,
+    output [7:0]    oLED,
+);
+endmodule
