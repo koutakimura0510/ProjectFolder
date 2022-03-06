c150587c600f9398252c788922836e6680295133
Kouta Kimura <afunfun5100@icloud.com>
2022-03-06 14:10:07 +0900

    microblaze

--- Xilinx/workspace/nexysVideo/microblaze/mcs/app.srcs/sources_1/new/main.v
+++ Xilinx/workspace/nexysVideo/microblaze/mcs/app.srcs/sources_1/new/main.v
@@ -1,35 +1,31 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2022/03/05 21:18:05
-// Design Name: 
-// Module Name: main
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+// Create 2021/3/6
+// Author koutakimura
+// Editor VSCode ver1.62.7
+// Build  Vivado20.2
+// Borad  Nexys Video
+// -
+// Micro Blaze MCS サンプル動作
+// 外部割り込み機能を使用して、ボード付属のLEDを点灯させる
+//----------------------------------------------------------
 
-
-module main(
+module main #(
+    parameter pMB = "on"
+)(
     input           iCLK,
     input           iRST,
     output          oUartTx,
     output [7:0]    oLED
 );
 
+
+//----------------------------------------------------------
+// Reset Active High、付属SWがプルダウンのため反転
+//----------------------------------------------------------
 wire rst = ~iRST;
 
 
+////////////////////////////////////////////////////////////
 //----------------------------------------------------------
 // 1秒単位で割り込み信号を生成
 //----------------------------------------------------------
@@ -44,23 +40,40 @@ enGen # (
 );
 
 ////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+// Micro Blaze mcs
+// Interrput Port に立ち上がりエッジを入力すると、割り込み処理開始としてIRQ PortがHighになる
+// ソフトウェア側で ACK レジスタをクリアし IRQ Port をLowにする
+// GPIO Portは直接 外部IOを駆動しているわけではなく、指定bit幅でデータのやり取りを行っているだけなので、
+// CPU 側の計算結果を RTLで受け取り、外部ハードウェアを動作させる記述にしなければならない。
+//----------------------------------------------------------
 wire [7:0] wLed;
 wire wIrq;
 
-microblaze_mcs_0 MB (
-    .Clk            (iCLK),
-    .Reset          (rst),
-    .INTC_IRQ       (wIrq),
-    .INTC_Interrupt (wEn),
-    .UART_txd       (oUartTx),
-    .GPIO1_tri_o    (wLed)
-);
+generate
+    if (pMB == "on")
+    begin
+        microblaze_mcs_0 MB (
+            .Clk            (iCLK),
+            .Reset          (rst),
+            .INTC_IRQ       (wIrq),
+            .INTC_Interrupt (wEn),
+            .UART_txd       (oUartTx),
+            .GPIO1_tri_o    (wLed)
+        );
+    end
+endgenerate
 
 
 ////////////////////////////////////////////////////////////
+//----------------------------------------------------------
+// LED 駆動
+//----------------------------------------------------------
 reg rMsbLed;
 reg [7:0] rLed;             assign oLED = {rMsbLed, rLed[6:0]};
 
+// 割り込み処理開始 port 受信
+// 8bit目のledを点滅させる
 always @(posedge iCLK)
 begin
     if (rst )       rMsbLed <= 1'b0;
@@ -68,6 +81,7 @@ begin
     else            rMsbLed <= rMsbLed;
 end
 
+// CPUの計算結果を受信し LEDに描画
 always @(posedge iCLK)
 begin
     if (rst)    rLed <= 8'hff;
