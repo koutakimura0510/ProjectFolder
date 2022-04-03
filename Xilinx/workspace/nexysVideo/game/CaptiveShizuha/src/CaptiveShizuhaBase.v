32e871b9fbc529c5a772757c9ab7e49d96c5586d
Kouta Kimura <afunfun5100@icloud.com>
2022-04-03 20:29:24 +0900

    [Base]
    oledブロックをTopに移動

--- Xilinx/workspace/nexysVideo/game/CaptiveShizuha/src/CaptiveShizuhaBase.v
+++ Xilinx/workspace/nexysVideo/game/CaptiveShizuha/src/CaptiveShizuhaBase.v
@@ -11,7 +11,8 @@
 module CaptiveShizuhaBase #(
     parameter       pHdisplay     = 640,
     parameter       pVdisplay     = 480,
-    parameter       pPixelDebug   = "yes"
+    parameter       pPixelDebug   = "yes",
+    parameter       pBuffDepth    = 1024
 )(
 
     input           iPCLK,      // Pixel Clk
@@ -22,21 +23,10 @@ module CaptiveShizuhaBase #(
     input  [5:0]    iBtn,       // Async Push SW
 
     // Pixel Clk Sync Signal
-    input           iPVde,      // Pixel Clk Timing video enable
     input           iPFvde,     // Pixel Clk Timing fast video enable
-    input           iPFe,       // Pixel Clk Timing frame end
 
     // TGB side output
-    output [23:0]   oVRGB,
-
-    // debug monitor
-    output          oOledScl,
-    output          oOledSda,
-    output          oOledDC,
-    output          oOledRes,
-    output          oOledVbat,
-    output          oOledVdd,
-    output [7:0]    oLED
+    output [23:0]   oVRGB
 );
 
 
@@ -53,17 +43,19 @@ module CaptiveShizuhaBase #(
 //----------------------------------------------------------
 // Dot Generate Block
 //----------------------------------------------------------
-reg qCkeDgb;
-wire [23:0] wPixel;
+reg  qCkeDgb;
+wire [23:0] wPiDgb;
+wire wVdDgb;
 
 dgbWrapper #(
     .pHdisplay          (pHdisplay),
     .pVdisplay          (pVdisplay),
-    .pPixelWidth        (24)
+    .pPixelWidth        (24),
+    .pPixelDebug        (pPixelDebug)
 ) DGB (
     .iBCLK  (iBCLK),    .iRST   (iRST),
-    .iCKE   (qCkeDgb),  .oPixel (wPixel),
-    .oVd    (),         .oFe    ()
+    .iCKE   (qCkeDgb),  .oPixel (wPiDgb),
+    .oVd    (wVdDgb),   .oFe    ()
 );
 
 
@@ -79,60 +71,24 @@ dgbWrapper #(
 //----------------------------------------------------------
 // Pixel Async Fifo Block
 //----------------------------------------------------------
-wire [23:0] wRD;        assign oVRGB = wRD;
+wire [23:0] wRD;            assign oVRGB = wRD;
 wire wFull;
 
 
 pfbWrapper #(
-    .pBuffDepth             (1024),
+    .pBuffDepth             (pBuffDepth),
     .pBitWidth              (24)
 ) PFB (
     .iBCLK  (iBCLK),        .iPCLK  (iPCLK),
-    .iRST   (iRST),     
-    .iWD    (wPixel),       .iWE    (qCkeDgb),
-    .oFull  (wFull),
-    .oRD    (wRD),          .iRE    (iPFvde)
+    .iRST   (iRST),
+    .iWD    (wPiDgb),       .oRD    (wRD),
+    .iWE    (wVdDgb),       .iRE    (iPFvde),
+    .oFull  (wFull)
 );
 
 always @*
 begin
     qCkeDgb <= (~wFull);
 end
-
-
-//----------------------------------------------------------
-// デバッグ用に値表示
-//----------------------------------------------------------
-odbWrapper #(
-    .PDIVCLK        (100000),
-    .PDIVSCK        (128),
-    .DISPLAY_WIDTH  (128),
-    .DISPLAY_HEIGHT (4),
-    // .BIT_LENGTH     (64)
-    .BIT_LENGTH     (95)
-) ODB (
-    .iCLK           (iBCLK),
-    .iRST           (iRST),
-    .oOledScl       (oOledScl),
-    .oOledSda       (oOledSda),
-    .oOledDC        (oOledDC),
-    .oOledRes       (oOledRes),
-    .oOledVbat      (oOledVbat),
-    .oOledVdd       (oOledVdd),
-    .iDispLine1     ({"XPOS =  ", 0}),
-    .iDispLine2     ({"YPOS =  ", 0}),
-    .iDispLine3     ({"        ", 0}),
-    .iDispLine4     ({"        ", 0})
-);
-
-reg [7:0] rLed;
-
-always @(posedge iBCLK)
-begin
-    if (iRST)   rLed <= 0;
-    else        rLed <= {2'b000, iBtn};
-end
-
-assign oLED = rLed;
 
 endmodule
