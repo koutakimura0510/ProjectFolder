4c37245ee260d73c7ab0ccaaab439f1db7c65c88
Kouta Kimura <afunfun5100@icloud.com>
2022-07-05 07:16:16 +0900

    [i2sSerializer] i2s信号出力回路製作中

--- Xilinx/workspace/Spartan7/BraveFrontier/src/PostProcessor/I2SAudioBlock/i2sSerializer.v
+++ Xilinx/workspace/Spartan7/BraveFrontier/src/PostProcessor/I2SAudioBlock/i2sSerializer.v
@@ -17,59 +17,52 @@ module i2sSerializer (
 
 
 //----------------------------------------------------------
-// MCLK Counter
+// BCLK Generator
 // MCLK = BCLK(MHz) * 8
 // 24.576MHz = 3.072MHz * 8
 // negedge だけの SDR なので 実際は 4cnt で検出する
 //----------------------------------------------------------
-localparam lpMclkCntMax = 4'd4; 
+localparam lpBclkCntMax = 4'd4; 
 
 reg  [2:0] rMclkCnt;
-wire wMclkCke;
+reg  rBclk;              assign oAudioBclk = rBclk;
+wire wBclkCke;
 
 always @(negedge iAudioClk)
 begin
     if (iAudioRst)      rMclkCnt <= 3'd0;
-    else if (wMclkCke)  rMclkCnt <= 3'd0;
+    else if (wBclkCke)  rMclkCnt <= 3'd0;
     else                rMclkCnt <= rMclkCnt + 1'd1;
+
+    if (iAudioRst)      rBclk <= 1'd0;
+    else if (wBclkCke)  rBclk <= ~rBclk;
+    else                rBclk <= rBclk;
 end
 
 always @*
 begin
-    wMclkCke <= (rMclkCnt == lpMclkCntMax);
-end
-
-
-//----------------------------------------------------------
-// BCLK Generator
-//----------------------------------------------------------
-reg rBclk;              assign oAudioBclk = rBclk;
-
-always @(negedge iAudioClk)
-begin
-    if (iAudioRst)      rBclk <= 1'd0;
-    else if (wMclkCke)  rBclk <= ~rBclk;
-    else                rBclk <= rBclk;
+    wBclkCke <= (rMclkCnt == lpBclkCntMax);
 end
 
 
 //----------------------------------------------------------
 // LRCLK
-// LRch 32bit MSB farst
 // Low  LCLK Output
 // High RCLK Output
+// Bclk の CKE が DDR でアサートされるので、32bit x 2clk
+// 合計 64カウント のレジスタを確保する
 //----------------------------------------------------------
-localparam lpCclkCntMax = 6'd31;
+localparam lpCclkCntMax = 7'd63;
 
 reg  rCclk;             assign oAudioCclk = rCclk;
-reg  [5:0] rCclkCnt;
+reg  [6:0] rCclkCnt;
 wire wCclkCke;
 
 always @(negedge iAudioClk)
 begin
-    if (iAudioRst)      rCclkCnt <= 6'd0;
-    else if (wCclkCke)  rCclkCnt <= 6'd0;
-    else                rCclkCnt <= rCclkCnt + 1'd1;
+    if (iAudioRst)      rCclkCnt <= 7'd0;
+    else if (wBclkCke)  rCclkCnt <= rCclkCnt + 1'd1;
+    else                rCclkCnt <= rCclkCnt;
 
     if (iAudioRst)      rCclk <= 1'd0;
     else if (wCclkCke)  rCclk <= ~rCclk;
@@ -84,7 +77,20 @@ end
 
 //----------------------------------------------------------
 // パラレル -> シリアル変換
+// LRch 32bit MSB farst
 //----------------------------------------------------------
+reg  rAudioData;           assign oAudioData = rAudioData;
+wire wBitRp;
 
+always @(negedge iAudioClk)
+begin
+    if (wCclkCke)  rAudioData <= iAudioData[0];
+    else           rAudioData <= iAudioData[wBitRp];
+end
+
+always @*
+begin
+    wBitRp <= ~(rCclkCnt[6:1]);
+end
 
 endmodule
