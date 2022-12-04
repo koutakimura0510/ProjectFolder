d9b0af631d27436384e9a29c200f31ffe3cc4643
kimura-XPS-Ubuntu <afunfun5100@icloud.com>
2022-12-05 07:42:42 +0900

    [update] DSP Sample

--- Xilinx/Sample/Dsp/Dsp.srcs/sources_1/new/DspTop.v
+++ Xilinx/Sample/Dsp/Dsp.srcs/sources_1/new/DspTop.v
@@ -1,26 +1,97 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/04/2022 09:05:36 PM
-// Design Name: 
-// Module Name: DspTop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/*
+Create  2022/12/04
+Author  kimura
+Editor  VSCode ver1.73.1
+Build   Vivado22.2
+Device  CmodA7
+-
+内蔵乗算機がちゃんと使用されるか、合成結果を確認するためのサンプル
 
+*/
 
 module DspTop(
+	output oLed,
+	input iClk,
+	input iRst
+);
 
-    );
+
+//-----------------------------------------------------------------------------
+// 乗算用計算値生成
+//-----------------------------------------------------------------------------
+reg [3:0] rDstR;
+reg [3:0] rDstG;
+reg [3:0] rDstB;
+
+always @( posedge iClk )
+begin
+	if (iRst)
+	begin
+		rDstR <= 4'hf;
+		rDstG <= 4'hf;
+		rDstB <= 4'hf;
+	end
+	else
+	begin
+		rDstR <= rDstR + 1'd1;
+		rDstG <= rDstG + 2'd2;
+		rDstB <= rDstB + 2'd3;
+	end
+end
+
+
+
+//-----------------------------------------------------------------------------
+// 内蔵乗算機を使用
+// この場合は 3つ使用される
+// (* use_dsp ="yes" *) wire[7:0] wSrcR = 4'd10 * rDstR;
+// (* use_dsp ="yes" *) wire[7:0] wSrcG = 4'd10 * rDstG;
+// (* use_dsp ="yes" *) wire[7:0] wSrcB = 4'd10 * rDstB;
+// 
+// (* use_dsp ="yes" *) reg[7:0] rDspR;
+// (* use_dsp ="yes" *) reg[7:0] rDspG;
+// (* use_dsp ="yes" *) reg[7:0] rDspB;
+// 
+// (* use_dsp ="yes" *) プリミティブをなくすと、LUT と FF が使用される。
+// ソースコード上では乗算だが、加算器をたくさん並べた回路に生成される。
+// 上記3つ使用をなくすと下記のリソースが増える。
+// LUT +8
+// FF +17 
+//-----------------------------------------------------------------------------
+wire[7:0] wSrcR = 4'd10 * rDstR;
+wire[7:0] wSrcG = 4'd10 * rDstG;
+wire[7:0] wSrcB = 4'd10 * rDstB;
+// (* use_dsp ="yes" *) reg[7:0] rDspR;
+// (* use_dsp ="yes" *) reg[7:0] rDspG;
+// (* use_dsp ="yes" *) reg[7:0] rDspB;
+
+reg [7:0] rSrcR;
+reg [7:0] rSrcG;
+reg [7:0] rSrcB;
+
+always @( posedge iClk )
+begin
+	if (iRst)
+	begin
+		rSrcR <= 8'hff;
+		rSrcG <= 8'hff;
+		rSrcB <= 8'hff;
+	end
+	else
+	begin
+		rSrcR <= wSrcR;
+		rSrcG <= wSrcG;
+		rSrcB <= wSrcB;
+		// rDspR <= 4'd10 * rDstR;
+		// rDspG <= 4'd10 * rDstG;
+		// rDspB <= 4'd10 * rDstB;
+		// rSrcR <= rDspR;
+		// rSrcG <= rDspG;
+		// rSrcB <= rDspB;
+	end
+end
+
+assign oLed = &{rSrcR} && &{rSrcG} && &{rSrcB};
+
 endmodule
