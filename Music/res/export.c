de906acb15c2de37683d816efcd35b183124069f
kimura-XPS-Ubuntu <afunfun5100@icloud.com>
2023-10-08 16:09:23 +0900

    末尾にヌル文字が挿入される不具合改善

--- Music/res/export.c
+++ Music/res/export.c
@@ -1,51 +1,89 @@
+/**-----------------------------------------------------------------------------
+ * Create  2023/10/01
+ * Author  kouta kimura
+ * -
+ * RAW ファイルを xxd コマンドを使用してダンプする
+ * RAW ファイルは 16bit signed を想定している
+ * 
+ * プログラム自体は2020年辺りに作成したもので、コメントが一切記載されてなかったので記載
+ * output ディレクトリには xxd コマンドでダンプしたファイルが出力される。
+ * sdfile には ダンプ出力したファイルを FILE_LINE_BYTE_NUMBER の数値分 1行にまとめたファイルが出力される。
+ * SDカード接続の マイコン等で読み込む場合は、RAMに制限があるので2048 など小さい数値を指定する。
+ * OS で使う場合は大きくても問題ない
+ * 
+ * 2023/10/01 new release
+ * 
+ *-----------------------------------------------------------------------------*/
+
+/**-----------------------------------------------------------------------------
+ * Include File
+ *-----------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 
+
+/**-----------------------------------------------------------------------------
+ * define
+ *-----------------------------------------------------------------------------*/
+#define FILE_LINE_BYTE_NUMBER (4096)
+
+
+/**-----------------------------------------------------------------------------
+ * 関数宣言
+ *-----------------------------------------------------------------------------*/
+static void error_fprintf(const char *s, int line);
+static void get_filename(char *path, char *filename);
+static void xxd_cmd(char *file, char *filename);
+static void file_generate(void);
+static int system_init(char *path);
+
+/**-----------------------------------------------------------------------------
+ * FILE ptr
+ *-----------------------------------------------------------------------------*/
 FILE *fp;
 FILE *read_fp;
 
 
-/*
- * エラー処理関数
- */
-void error_fprintf(const char *s, int line)
+/**-----------------------------------------------------------------------------
+ * エラー発生時のターミナル表示関数
+ *-----------------------------------------------------------------------------*/
+static void error_fprintf(const char *s, int line)
 {
 	fprintf(stderr, "[Line] = %d\n", line);
 	fprintf(stderr, "[func] = %s\n", s);
 }
 
 
-/*
+/**-----------------------------------------------------------------------------
  * ディレクトリのパスとファイル名を取得
- *
- * パスを指定
- * raw/battle/battle.raw
  * 
- * filenameに取得
- * battle/battle.raw
- */
-void get_filename(char *path, char *filename)
+ * *path = ダンプを行う raw ファイルパスを入力する, 例: raw/battle/hogehoge.raw
+ * *filename = 出力先のファイル名をコピー 例: battle/hogehoge.raw
+ *-----------------------------------------------------------------------------*/
+static void get_filename(char *path, char *filename)
 {
 	uint32_t count = 0;
     uint32_t n = 0;
 
-	do
-	{
+	// .raw 拡張子までアドレスを進める
+	do {
 		path++;
-        if (*path == '/') {
-            n++;
-        }
+		if (*path == '/') {
+			n++;
+		}
 	} while (*path != '.');
 
-    for (int i = 0; i < n; i++) {
-        do
-        {
-            path--;
-        } while (*path != '/');
-    }
+	// path アドレスが拡張子まで進んでいるので、
+	// ファイル名の開始までアドレスを戻す
+	for (int i = 0; i < n; i++) {
+		do {
+			path--;
+		} while (*path != '/');
+	}
 
+	// ファイル名のコピー
 	path++;
 	while (path[count] != '\0')
 	{
@@ -55,12 +93,15 @@ void get_filename(char *path, char *filename)
 }
 
 
-/*
+/**-----------------------------------------------------------------------------
  * rawファイルをxxdコマンドでダンプ
  *
  * リダイレクトコマンドはファイルに追加動作をしてしまうため
  * 既存のファイルを消去してから出力を行う
- */
+ * 
+ * *path = ダンプを行う raw ファイルパスを入力する, 例: raw/battle/hogehoge.raw
+ * *filename = 出力のファイル名 例: hogehoge.raw
+ *-----------------------------------------------------------------------------*/
 void xxd_cmd(char *file, char *filename)
 {
 	char rm[128]  = "rm ./output/";
@@ -83,10 +124,35 @@ void xxd_cmd(char *file, char *filename)
     }
 }
 
+/**-----------------------------------------------------------------------------
+ * ファイル生成
+ *-----------------------------------------------------------------------------*/
+void file_generate(void)
+{
+    int count = 0;
+    int s;
+    unsigned char c = '\n';
 
-/*
- * 初期設定関数
- */
+    while (!feof(read_fp))
+    {
+        s = fgetc(read_fp);
+
+        if (s != '\n') {
+            fwrite(&s, sizeof(unsigned char), 1, fp);
+            count++;
+        }
+
+        if (count == FILE_LINE_BYTE_NUMBER) {
+            count = 0;
+            fwrite(&c, sizeof(unsigned char), 1, fp);
+        }
+    }
+}
+
+
+/**-----------------------------------------------------------------------------
+ * 初期設定関数 
+ *-----------------------------------------------------------------------------*/
 int system_init(char *path)
 {
 	char dir_path[256] = "./sdfile/";
@@ -113,7 +179,7 @@ int system_init(char *path)
 
     fprintf(stderr, "読み込みファイル = %s\n", out_path);
 	strcat(dir_path, filename);
-	fp = fopen(dir_path, "w");
+	fp = fopen(dir_path, "r+");
 
 	if (fp == NULL) {
 		fprintf(stderr, "File Open error\n");
@@ -124,41 +190,29 @@ int system_init(char *path)
 	fprintf(stderr, "ファイルを生成します = %s\n", dir_path);
 	fprintf(stderr, "\n");
 
+	// File コピーして配置整列
+	file_generate();
+
+	// ファイルの末尾文字を削除
+    fseek(fp, -1, SEEK_END);
+    ftruncate(fileno(fp), ftell(fp));
+	fclose(fp);
+
 	return 0;
 }
 
 
-void file_generate(void)
-{
-    int count = 0;
-    int s;
-    unsigned char c = '\n';
-
-    while (!feof(read_fp))
-    {
-        s = fgetc(read_fp);
-
-        if (s != '\n') {
-            fwrite(&s, sizeof(unsigned char), 1, fp);
-            count++;
-        }
-
-        if (count == 3840) {
-            count = 0;
-            fwrite(&c, sizeof(unsigned char), 1, fp);
-        }
-    }
-}
-
-
+/**-----------------------------------------------------------------------------
+ * main 関数
+ *-----------------------------------------------------------------------------*/
 int main(int argc, char **argv)
 {
     if (1 == system_init(argv[1])) {
         return 1;
     }
 
-    file_generate();
-    fclose(fp);
+    // file_generate();
+    // fclose(fp);
     // fclose(read_fp);
 
     return 0;
--- Music/res/readme.txt
+++ Music/res/readme.txt
@@ -1,13 +1,16 @@
 rawファイル作成手順
 
 formerフォルダからrawフォルダに音源保存
-1. audacity　実行
+1. audacity 実行
 2. ステレオからモノラルにトラック変更
 3. サンプリングレートを指定に変更
 4. ファイル書き出し
 5. RAWファイル(header-less)
-6. Unsigned 8-bit PCM
+6. Unsigned 8-bit PCM, もしくは Signed 16bit PCM
 
 
 プログラムを実行してrawフォルダの音源をoutputとsdfileフォルダに保存
 1. 引数にrawフォルダの形成したい音源ファイルを指定する
+
+output ファイルに生成されるファイルは、export.c 内の count で改行される。
+Zynq を使用したプロジェクトの名残。
